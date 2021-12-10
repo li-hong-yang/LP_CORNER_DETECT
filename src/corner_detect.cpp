d983dfe0812a6d0b95daf68798de0911c2999e5e
lihongyang <lihongyang@hongyang.li>
2021-12-09 22:25:10 -0800

    add model decode utils

--- src/corner_detect.cpp
+++ src/corner_detect.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <iomanip>
 #include <cmath>
+#include "decode_utils.h"
 
 static Logger gLogger;
 
@@ -92,120 +93,6 @@ void CornerDetect::preprocess(string& img_name)
 
 }
 
-bool cmp(const Yolo::Detection& a, const Yolo::Detection& b) {
-    return a.conf > b.conf;
-}
-
-float iou(float lbox[4], float rbox[4]) {
-    float interBox[] = {
-        (std::max)(lbox[0] - lbox[2] / 2.f , rbox[0] - rbox[2] / 2.f), //left
-        (std::min)(lbox[0] + lbox[2] / 2.f , rbox[0] + rbox[2] / 2.f), //right
-        (std::max)(lbox[1] - lbox[3] / 2.f , rbox[1] - rbox[3] / 2.f), //top
-        (std::min)(lbox[1] + lbox[3] / 2.f , rbox[1] + rbox[3] / 2.f), //bottom
-    };
-
-    if (interBox[2] > interBox[3] || interBox[0] > interBox[1])
-        return 0.0f;
-
-    float interBoxS = (interBox[1] - interBox[0])*(interBox[3] - interBox[2]);
-    return interBoxS / (lbox[2] * lbox[3] + rbox[2] * rbox[3] - interBoxS);
-}
-
-
-cv::Rect get_rect(float bbox[4]) {
-    int x = int(bbox[0]);
-    int y = int(bbox[1]);
-    int w = int(bbox[2]-bbox[0]);
-    int h = int(bbox[3]-bbox[1]);
-
-    
-    return cv::Rect(x,y,w,h);
-}
-
-
-float* DecodeBbox(float* loc,float* priors,vector<float>& variances,int w,int h)
-{
-
-
-
-    float* bbox = new float[4];
-    float* dst = new float[4];
-
-    bbox[0] = priors[0] + loc[0] * variances[0] * priors[2];
-    bbox[1] = priors[1] + loc[1] * variances[0] * priors[3];
-
-    bbox[2] = priors[2] * exp(loc[2]*variances[1]);
-    bbox[3] = priors[3] * exp(loc[3]*variances[1]);
-
-    dst[0] = (bbox[0] - bbox[2]/2.0);
-    dst[1] = (bbox[1] - bbox[3]/2.0);
-    dst[2] = (bbox[0] + bbox[2]/2.0);
-    dst[3] = (bbox[1] + bbox[3]/2.0);
-
-    dst[0] *= 416;
-    dst[1] *= 416;
-    dst[2] *= 416;
-    dst[3] *= 416;
-    return dst;
-    
-}
-
-
-float* DecodeCorner(float* loc,float* priors,vector<float>& variances)
-{
-
-
-
-    float* bbox = new float[8];
-
-    bbox[0] = priors[0] + loc[0] * variances[0] * priors[2];
-    bbox[1] = priors[1] + loc[1] * variances[0] * priors[3];
-
-    bbox[2] = priors[0] + loc[2] * variances[0] * priors[2];
-    bbox[3] = priors[1] + loc[3] * variances[0] * priors[3];
-
-    bbox[4] = priors[0] + loc[4] * variances[0] * priors[2];
-    bbox[5] = priors[1] + loc[5] * variances[0] * priors[3];
-
-    bbox[6] = priors[0] + loc[6] * variances[0] * priors[2];
-    bbox[7] = priors[1] + loc[7] * variances[0] * priors[3];
-
-   
-    bbox[0] *= 416;
-    bbox[1] *= 416;
-    bbox[2] *= 416;
-    bbox[3] *= 416;
-    bbox[4] *= 416;
-    bbox[5] *= 416;
-    bbox[6] *= 416;
-    bbox[7] *= 416;
-    return bbox;
-    
-}
-
-
-
-cv::Mat get_perspective_mat(float corner[])
-{
-	cv::Point2f src_points[] = { 
-		cv::Point2f(int(corner[4]), int(corner[5])),
-		cv::Point2f(int(corner[6]), int(corner[7])),
-		cv::Point2f(int(corner[2]), int(corner[3])),
-		cv::Point2f(int(corner[0]), int(corner[1])) };
- 
-	cv::Point2f dst_points[] = {
-		cv::Point2f(0, 0),
-		cv::Point2f(94,0),
-		cv::Point2f(0, 24),
-		cv::Point2f(94, 24) };
- 
-	cv::Mat M = cv::getPerspectiveTransform(src_points, dst_points);
-	
-	return M;
- 
-}
-
-
 
 void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thresh,string& bbox)
 {
@@ -229,8 +116,7 @@ void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thre
     int imgh = img.rows;
     int imgw = img.cols;
 
-    for (int i = 0;i < output_size/det_size; i++) {
-        
+    for (int i = 0;i < output_size/det_size; i++) {      
         if (output_buffer[det_size * i + 5] <= conf_thresh) continue;
         Yolo::Detection det;
         float loc[4];
@@ -238,9 +124,13 @@ void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thre
         float pre[8];
         float* xyxy;
         float* cooners;
-        memcpy(&loc, &output_buffer[det_size * i], 4*sizeof(float));
-        memcpy(&priors, (char*)decode_bbox+sizeof(float)*4*i, 4*sizeof(float));
-        memcpy(&pre, &output_buffer[det_size*i+6], 8*sizeof(float));
+
+        // vector<float> m1(output_buffer,output_buffer+4);
+        // cout << m1[0] << endl;
+
+        memcpy(loc, &output_buffer[det_size * i], 4*sizeof(float));
+        memcpy(priors, (char*)decode_bbox+sizeof(float)*4*i, 4*sizeof(float));
+        memcpy(pre, &output_buffer[det_size*i+6], 8*sizeof(float));
         
         xyxy = DecodeBbox(loc,priors,variances,imgw,imgh);
         cooners = DecodeCorner(pre,priors,variances);
@@ -276,14 +166,15 @@ void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thre
             // cout << pre[7] << endl;
 
 
-            cout << cooners[0] << endl;
-            cout << cooners[1] << endl;
-            cout << cooners[2] << endl;
-            cout << cooners[3] << endl;
-            cout << cooners[4] << endl;
-            cout << cooners[5] << endl;
-            cout << cooners[6] << endl;
-            cout << cooners[7] << endl;
+            // cout << cooners[0] << endl;
+            // cout << cooners[1] << endl;
+            // cout << cooners[2] << endl;
+            // cout << cooners[3] << endl;
+            // cout << cooners[4] << endl;
+            // cout << cooners[5] << endl;
+            // cout << cooners[6] << endl;
+            // cout << cooners[7] << endl;
+
             // cout << loc[i+3] << endl;
 
             // cout << priors[i+0] << endl;
@@ -327,14 +218,14 @@ void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thre
     }
     cv::imwrite("lp.jpg", imgraw);
 
-    cout << res[0].corner[0] << endl;
-    cout << res[0].corner[1] << endl;
-    cout << res[0].corner[2] << endl;
-    cout << res[0].corner[3] << endl;
-    cout << res[0].corner[4] << endl;
-    cout << res[0].corner[5] << endl;
-    cout << res[0].corner[6] << endl;
-    cout << res[0].corner[7] << endl;
+    // cout << res[0].corner[0] << endl;
+    // cout << res[0].corner[1] << endl;
+    // cout << res[0].corner[2] << endl;
+    // cout << res[0].corner[3] << endl;
+    // cout << res[0].corner[4] << endl;
+    // cout << res[0].corner[5] << endl;
+    // cout << res[0].corner[6] << endl;
+    // cout << res[0].corner[7] << endl;
 
     cv::Mat pers_img;
     cv::Mat out_img;
@@ -342,6 +233,7 @@ void CornerDetect::postprocess(string& img_name,float conf_thresh,float nms_thre
     cv::warpPerspective(imgraw,out_img,pers_img,cv::Size(94,24));
 
     cv::imwrite("pers_lp.jpg", out_img);
+    cout << "save_done" << endl;
 
 
 
---
+++ src/decode_utils.cpp
@@ -0,0 +1,108 @@
+#include "decode_utils.h"
+
+
+cv::Mat get_perspective_mat(float corner[])
+{
+	cv::Point2f src_points[] = { 
+		cv::Point2f(int(corner[4]), int(corner[5])),
+		cv::Point2f(int(corner[6]), int(corner[7])),
+		cv::Point2f(int(corner[2]), int(corner[3])),
+		cv::Point2f(int(corner[0]), int(corner[1])) };
+ 
+	cv::Point2f dst_points[] = {
+		cv::Point2f(0, 0),
+		cv::Point2f(94,0),
+		cv::Point2f(0, 24),
+		cv::Point2f(94, 24) };
+ 
+	cv::Mat M = cv::getPerspectiveTransform(src_points, dst_points);
+	
+	return M;
+ 
+}
+
+cv::Rect get_rect(float bbox[4]) {
+    int x = int(bbox[0]);
+    int y = int(bbox[1]);
+    int w = int(bbox[2]-bbox[0]);
+    int h = int(bbox[3]-bbox[1]); 
+    return cv::Rect(x,y,w,h);
+}
+
+
+float* DecodeCorner(float* loc,float* priors,vector<float>& variances)
+{
+
+
+
+    float* bbox = new float[8];
+
+    bbox[0] = priors[0] + loc[0] * variances[0] * priors[2];
+    bbox[1] = priors[1] + loc[1] * variances[0] * priors[3];
+
+    bbox[2] = priors[0] + loc[2] * variances[0] * priors[2];
+    bbox[3] = priors[1] + loc[3] * variances[0] * priors[3];
+
+    bbox[4] = priors[0] + loc[4] * variances[0] * priors[2];
+    bbox[5] = priors[1] + loc[5] * variances[0] * priors[3];
+
+    bbox[6] = priors[0] + loc[6] * variances[0] * priors[2];
+    bbox[7] = priors[1] + loc[7] * variances[0] * priors[3];
+
+   
+    bbox[0] *= 416;
+    bbox[1] *= 416;
+    bbox[2] *= 416;
+    bbox[3] *= 416;
+    bbox[4] *= 416;
+    bbox[5] *= 416;
+    bbox[6] *= 416;
+    bbox[7] *= 416;
+    return bbox;
+    
+}
+
+bool cmp(const Yolo::Detection& a, const Yolo::Detection& b) {
+    return a.conf > b.conf;
+}
+
+float iou(float lbox[4], float rbox[4]) {
+    float interBox[] = {
+        (std::max)(lbox[0] - lbox[2] / 2.f , rbox[0] - rbox[2] / 2.f), //left
+        (std::min)(lbox[0] + lbox[2] / 2.f , rbox[0] + rbox[2] / 2.f), //right
+        (std::max)(lbox[1] - lbox[3] / 2.f , rbox[1] - rbox[3] / 2.f), //top
+        (std::min)(lbox[1] + lbox[3] / 2.f , rbox[1] + rbox[3] / 2.f), //bottom
+    };
+
+    if (interBox[2] > interBox[3] || interBox[0] > interBox[1])
+        return 0.0f;
+
+    float interBoxS = (interBox[1] - interBox[0])*(interBox[3] - interBox[2]);
+    return interBoxS / (lbox[2] * lbox[3] + rbox[2] * rbox[3] - interBoxS);
+}
+
+
+
+float* DecodeBbox(float* loc,float* priors,vector<float>& variances,int w,int h)
+{
+    float* bbox = new float[4];
+    float* dst = new float[4];
+
+    bbox[0] = priors[0] + loc[0] * variances[0] * priors[2];
+    bbox[1] = priors[1] + loc[1] * variances[0] * priors[3];
+
+    bbox[2] = priors[2] * exp(loc[2]*variances[1]);
+    bbox[3] = priors[3] * exp(loc[3]*variances[1]);
+
+    dst[0] = (bbox[0] - bbox[2]/2.0);
+    dst[1] = (bbox[1] - bbox[3]/2.0);
+    dst[2] = (bbox[0] + bbox[2]/2.0);
+    dst[3] = (bbox[1] + bbox[3]/2.0);
+
+    dst[0] *= 416;
+    dst[1] *= 416;
+    dst[2] *= 416;
+    dst[3] *= 416;
+    return dst;
+    
+}
